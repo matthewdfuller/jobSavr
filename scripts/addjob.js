b5473a17c0eb3719c34fe3b3fb3225e55b2c81ae
Matt Fuller <matthewdf10@gmail.com>
2012-08-03 23:20:30 -0700

    design updates
    updated main page design

--- scripts/addjob.js
+++ scripts/addjob.js
@@ -1,3 +1,26 @@
 function addJob() {
     document.getElementById('add_job_outer_box').style.display="block";
+    var visible = true;
 }
+
+/*
+function monitorClick(e){
+        var evt = (e)?e:event;
+       
+        var theElem = (evt.srcElement)?evt.srcElement:evt.target;
+
+        while(theElem!=null) {
+            if(visible==true && theElem.id != "add_job_outer_box") {
+               document.getElementById('add_job_outer_box').style.display == 'none';
+                visible = false;
+                return true;
+            }  
+        }
+        }
+        
+        return true;
+}
+
+document.onclick = monitorClick;
+
+*/
