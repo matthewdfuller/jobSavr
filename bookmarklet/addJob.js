3059af73d65f5de744925364f1b90085251c26a8
Stephen Crane <scrane@mozilla.com>
2012-08-04 13:37:01 -0700

    Working on bookmarklet

--- bookmarklet/addJob.js
+++ bookmarklet/addJob.js
@@ -1,5 +1,5 @@
-var appURL = 'http://localhost/';
-var backendURL = 'http://localhost/backend/';
+var appURL = 'https://rinon.info/jobsavr';
+var backendURL = 'https://rinon.info/jobsavr/backend/index.php';
 
 var s=document.createElement('script');
 s.setAttribute('type','text/javascript');
@@ -7,37 +7,51 @@ s.setAttribute('charset','UTF-8');
 s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
 document.documentElement.appendChild(s);
 
-var popupRequest = new XMLHttpRequest();
-popupRequest.open('GET', appURL+'/bookmarklet/addjob.html', true);
-popupRequest.addEventListener('load', popupRetrieved);
-popupRequest.send();
+// var popupRequest = new XMLHttpRequest();
+// popupRequest.open('GET', appURL+'/bookmarklet/addjob.html', true);
+// popupRequest.addEventListener('load', popupRetrieved);
+// popupRequest.send();
 
-function popupRetrieved() {
-    console.log(popupRequest.responseText);
+var popup = document.createElement('div');
+popup.style.width = '30em';
+popup.style['margin-left'] = 'auto';
+popup.style['margin-right'] = 'auto';
+popup.style['background-color'] = '#EDEDED';
+popup.style['padding'] = '10px';
+popup.style['text-align'] = 'center';
+popup.style['-moz-box-shadow'] = '0 0 5px 5px #888';
+popup.style['-webkit-box-shadow'] = '0 0 5px 5px#888';
+popup.style['box-shadow'] = '0 0 5px 5px #888';
+popup.style['font-family'] = '\'Ruda\', sans-serif';
+popup.style['font-weight'] = '200';
+popup.style['color'] = '#303030';
+popup.style['line-height'] = '1.8';
+var iframe = document.createElement('iframe');
+iframe.src = appURL+'/bookmarklet/addjob.html';
+iframe.style.width = '30em';
+popup.appendChild(iframe);
 
-    var popup = document.createElement('div');
-    popup.innerHTML = popupRequest.responseText;
-    $("input[name='com_url']", popup).val(window.location);
-    $('body').prepend(popup);
-    $('#jobsavr_add_job').submit(function() {
-        $.ajax({
-	        type: 'POST',
-	        contentType: 'application/json;charset=UTF-8',
-	        url: backendURL,
-	        dataType: 'json',
-	        data: formToJSON(),
-	        success: function(data, textStatus, jqXHR){
-                if (handleError(data)) return;
-	        },
-	        error: function(jqXHR, textStatus, errorThrown){
-		        //alert('add job error: ' + textStatus);
-                document.getElementById('error_outer_box').innerHTML="";
-	        }
-        });
-        return false;
-        $(popup).remove();
+
+$("input[name='com_url']", popup).val(window.location);
+$('body').prepend(popup);
+$('#jobsavr_add_job').submit(function() {
+    $.ajax({
+	    type: 'POST',
+	    contentType: 'application/json;charset=UTF-8',
+	    url: backendURL,
+	    dataType: 'json',
+	    data: formToJSON(),
+	    success: function(data, textStatus, jqXHR){
+            if (handleError(data)) return;
+	    },
+	    error: function(jqXHR, textStatus, errorThrown){
+		    //alert('add job error: ' + textStatus);
+            document.getElementById('error_outer_box').innerHTML="";
+	    }
     });
-};
+    return false;
+    $(popup).remove();
+});
 
 function formToJSON() {
     return JSON.stringify({
