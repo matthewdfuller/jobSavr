84047997bb1f4d6096b2e24c0edb96eaca1df57b
Stephen Crane <scrane@mozilla.com>
2012-08-04 14:43:03 -0700

    fixed url

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,4 +1,4 @@
-var bookmarklet = "(function(){var url='http://localhost/hackday';var s=document.createElement('script');s.setAttribute('type','text/javascript');s.setAttribute('charset','UTF-8');s.setAttribute('src',url+'/bookmarklet/addJob.js');document.documentElement.appendChild(s);})();";
+var bookmarklet = "(function(){var url='http://localhost';var s=document.createElement('script');s.setAttribute('type','text/javascript');s.setAttribute('charset','UTF-8');s.setAttribute('src',url+'/bookmarklet/addJob.js');document.documentElement.appendChild(s);})();";
 
 $('#bookmarklet').ready(function() {
   $('#bookmarklet').attr('href', 'javascript:' + encodeURIComponent(bookmarklet).replace(/\(/g, '%28').replace(/\)/g, '%29'));
