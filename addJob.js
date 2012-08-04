d9cee97bbf572ad565452728aebe31456c5fb2e0
Stephen Crane <scrane@mozilla.com>
2012-08-03 22:53:01 -0700

    Removes LI JSAPI from bookmarklet page.
    We can't request from the API from just any page on the net

--- addJob.js
+++ addJob.js
@@ -6,13 +6,6 @@ s.setAttribute('charset','UTF-8');
 s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
 document.documentElement.appendChild(s);
 
-s=document.createElement('script');
-s.setAttribute('type','text/javascript');
-s.setAttribute('charset','UTF-8');
-s.setAttribute('src','http://platform.linkedin.com/in.js');
-s.innerHTML = 'api_key: l3bpklmxvfcp\nauthorize: true';
-document.documentElement.appendChild(s);
-
 var popupRequest = new XMLHttpRequest();
 popupRequest.open('GET', appURL+'/addjob.html', true);
 popupRequest.addEventListener('load', popupRetrieved);
