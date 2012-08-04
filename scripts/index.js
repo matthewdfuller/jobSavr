a66ec4a6b4295bbe358b71d7308ee301b95546c8
Matt Fuller <matthewdf10@gmail.com>
2012-08-04 14:11:15 -0700

    add ajax

--- scripts/index.js
+++ scripts/index.js
@@ -72,6 +72,12 @@ function addJob() {
 
 
 function updateJob() {
+    updateAjax();
+    
+    getJobs();
+}
+
+function updateAjax() {
     $.ajax({
         type: 'PUT',
 	contentType: 'application/json',
@@ -85,8 +91,6 @@ function updateJob() {
 		    alert('update job error: ' + textStatus);
         }
     });
-    
-    getJobs();
 }
 
 function deleteJob() {
