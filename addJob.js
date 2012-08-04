397721b89d5b33e1b071d4307b9505ac475a7c71
Stephen Crane <scrane@mozilla.com>
2012-08-03 22:36:27 -0700

    Fixing bookmarklet to use overlay instead of popup

--- addJob.js
+++ addJob.js
@@ -1,5 +1,28 @@
 var appURL = 'http://localhost/hackday';
 
-var addPopup = window.open(appURL+'/addjob.html','addJob','height=200,width=150');
-if (window.focus)
-  addPopup.focus();
+var s=document.createElement('script');
+s.setAttribute('type','text/javascript');
+s.setAttribute('charset','UTF-8');
+s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
+document.documentElement.appendChild(s);
+
+s=document.createElement('script');
+s.setAttribute('type','text/javascript');
+s.setAttribute('charset','UTF-8');
+s.setAttribute('src','http://platform.linkedin.com/in.js');
+s.innerHTML = 'api_key: l3bpklmxvfcp\nauthorize: true';
+document.documentElement.appendChild(s);
+
+var popupRequest = new XMLHttpRequest();
+popupRequest.open('GET', appURL+'/addjob.html', true);
+popupRequest.addEventListener('load', popupRetrieved);
+popupRequest.send();
+
+function popupRetrieved() {
+  console.log(popupRequest.responseText);
+
+  var popup = document.createElement('div');
+  popup.innerHTML = popupRequest.responseText;
+  $("input[name='com_url']", popup).val(window.location);
+  $('body').prepend(popup);
+};
--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,4 @@
-var bookmarklet = "(function(){var url='http://localhost/hackday';var s=document.createElement('script');s.setAttribute('type','text/javascript');s.setAttribute('charset','UTF-8');s.setAttribute('src',url+'/addJob.js');document.documentElement.appendChild(s);})();";
+var bookmarklet = "(function(){window.baseUrl='http://localhost';var s=document.createElement('script');s.setAttribute('type','text/javascript');s.setAttribute('charset','UTF-8');s.setAttribute('src',baseUrl+'/hackday/addJob.js');document.documentElement.appendChild(s);})();";
 
 $('#bookmarklet').ready(function() {
   $('#bookmarklet').attr('href', 'javascript:' + encodeURIComponent(bookmarklet).replace(/\(/g, '%28').replace(/\)/g, '%29'));
